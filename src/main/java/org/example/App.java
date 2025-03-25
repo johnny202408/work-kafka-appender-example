905ec8f94cbba583388afa2b3a86eb7ed4368189
johnny202408 <johnny.bgood24@proton.me>
2025-03-25 16:48:40 +0200

    kafka appender with params

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -23,7 +23,7 @@ public class App {
         System.out.println(ExampleCalculator.sum(1, 2));
 //        System.out.println("Consuming topic: [" + SOURCE_TOPIC + "] DLQ: [" + DLQ_TOPIC + "]");
         System.out.println("--------------------------------");
-        for (int i = 1; i < 20 ; i++) {
+        for (int i = 1; i < 12 ; i++) {
             MDC.put(sourceField, String.format("%d", i));
             MDC.put(typeField, String.format("%d", (i + 10)));
             logger.debug("Debugging stuff... ");
